f6eed7aa7a20ddb81550d17b61f61d698cfc4d27
Tony Alejandro <tonysviews@gmail.com>
2019-11-22 14:29:15 +0800

    remove reference to CDN resource

--- mv-pagination.js
+++ mv-pagination.js
@@ -1,10 +1,6 @@
-import {
-  LitElement,
-  html,
-  css
-} from "https://cdn.jsdelivr.net/gh/manaty/mv-dependencies@master/web_modules/lit-element.js";
+import { LitElement, html, css } from "lit-element";
 
-import "./web_modules/mv-button/mv-button.js";
+import "mv-button";
 
 export class MvPagination extends LitElement {
   static get properties() {
