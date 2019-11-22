33832718a06827502bc985b25ddea948990f95ff
Tony Alejandro <tonysviews@gmail.com>
2019-11-22 10:18:41 +0800

    add sub-directory to import statement

--- mv-pagination.js
+++ mv-pagination.js
@@ -4,7 +4,7 @@ import {
   css
 } from "https://cdn.jsdelivr.net/gh/manaty/mv-dependencies@master/web_modules/lit-element.js";
 
-import "./web_modules/mv-button.js";
+import "./web_modules/mv-button/mv-button.js";
 
 export class MvPagination extends LitElement {
   static get properties() {
